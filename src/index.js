a4e113a8d4af13034ec2957c8c534add7d6658f7
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-10-13 09:06:06 -0600

    create router and home files stub out basic router file

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@
 
 import ReactDOM from 'react-dom'
 import React from 'react'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { BrowserRouter as Router } from 'react-router-dom' 
 
 import App from './components/App'
 import { rootReducer } from './reducers/rootReducer'
