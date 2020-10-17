63fe61e8ed8b5137e205fd0f3e94104e3bddc097
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-10-17 08:08:38 -0600

    remove bootstrap and switch to material ui

--- src/components/EventsContainer.js
+++ src/components/EventsContainer.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import EventsList from "./EventsList"
 import EventsForm from "./EventsForm"
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+
 
 export class EventsContainer extends Component {
 
@@ -11,14 +9,8 @@ export class EventsContainer extends Component {
     render() {
         return (
             <div>
-                <Container fluid>
-                    <Row justify-content-center="true">
-                        <Col><EventsList /></Col>
-                    </Row>
-                    <Row>
-                        <Col><EventsForm /></Col>
-                    </Row>
-                </Container>
+                <EventsList />
+                <EventsForm />
             </div>
         )
     }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,26 +4,28 @@ import { NavLink } from "react-router-dom";
 export class Navbar extends Component {
   render() {
     return (
-      <div className="NavBar">
-        <ul>
-          <li>
-            {" "}
-            <NavLink to="/">Home</NavLink>{" "}
-          </li>
-          <li>
-            {" "}
-            <NavLink to="/events">Events</NavLink>{" "}
-          </li>
-          <li>
-            {" "}
-            <NavLink to="/about">About</NavLink>{" "}
-          </li>
-          <li>
-            {" "}
-            <NavLink to="/new">New Event</NavLink>{" "}
-          </li>
-        </ul>
-      </div>
+      <Grid className="NavBar">
+            <NavLink to="/">
+              
+              Home
+
+              </NavLink>
+            <NavLink to="/events">
+
+              Events
+
+              </NavLink>
+            <NavLink to="/about">
+
+              About
+
+              </NavLink>
+            <NavLink to="/new">
+
+              New Event
+
+              </NavLink>
+      </Grid>
     );
   }
 }
