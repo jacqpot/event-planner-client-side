cd8ea5ee6e9bc7c59d8eeed1ea8ef5727a9ea236
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-10-19 12:14:56 -0600

    updating home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import { Container } from "@material-ui/core";
+import React from "react";
 
 const Home = () => {
-    
-        return (
-            <div>
-                <h1>this is the home page</h1>
-                <p>Welcome to The best event planning app to date.</p>
-            </div>
-        )
-}
+  return (
+    <Container
+      fixed
+      maxWidth="sm"
+      style={{ backgroundColor: "#cfe8fc", height: "40vh" }}
+    >
+      <div className="home">
+        <h1>Event Planning</h1>
+        <p>
+          This App was developed too help lay-peoples plan church events. With a
+          simple, easy to follow process. This was done with my Youth group in
+          mind. There are several things that can be implemented to level up
+          this application.
+        </p>
+      </div>
+      <ul>
+        <li>
+          <p>add validations to input </p>
+        </li>
+        <li>
+          <p>
+            implement constraints for adding in new sections of an event, so
+            that no errors can be made{" "}
+          </p>
+        </li>
+        <li>
+          <p>full crud on events and their sections </p>
+        </li>
+        <li>
+          <p>Templates for a given recuring event like "Youth Group" </p>
+        </li>
+      </ul>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
