9557f964b8e497ab5cfffa1e17388d6e818df440
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-12-16 11:53:39 -0700

    adding classic css style, header and footer sized and spaced, body content in two collumns

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,19 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
 import "../style/App.css";
 export class Navbar extends Component {
   render() {
     return (
       <div className="NavBar">
-        <Grid>
-          <h1 className="title">Event Planner</h1>
+        <h1 className="title">Event Planner</h1>
+        <div>
           <Button href="/" variant="contained" color="primary">
             Home
           </Button>
           <Button href="/events" variant="contained" color="primary">
             Events
           </Button>
-        </Grid>
+        </div>
       </div>
     );
   }
