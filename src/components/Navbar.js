1d7cef7400e385eed67b6ff1e62a77afc6ca8cd6
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-12-16 11:59:34 -0700

    changed button color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,10 +7,10 @@ export class Navbar extends Component {
       <div className="NavBar">
         <h1 className="title">Event Planner</h1>
         <div>
-          <Button href="/" variant="contained" color="primary">
+          <Button href="/" variant="contained" color="#58694">
             Home
           </Button>
-          <Button href="/events" variant="contained" color="primary">
+          <Button href="/events" variant="contained" color="#58694">
             Events
           </Button>
         </div>
