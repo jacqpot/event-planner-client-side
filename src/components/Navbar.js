c2a6023a1e5087adb64e732c36e536cc3732f93a
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2021-01-12 15:06:07 -0700

    finish migrating base scss files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,10 +7,10 @@ export class Navbar extends Component {
       <div className="NavBar">
         <h1 className="title">Event Planner</h1>
         <div>
-          <Button href="/" variant="contained" color="#58694">
+          <Button href="/" class="btn btn--white btn--animated">
             Home
           </Button>
-          <Button href="/events" variant="contained" color="#58694">
+          <Button href="/events" class="btn btn--white btn--animated">
             Events
           </Button>
         </div>
